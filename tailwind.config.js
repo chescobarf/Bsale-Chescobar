0a4daea81ed0e847e3a43312a9f45baa597cab7e
Christian Escobar F <christian17zxe@gmail.com>
2022-01-07 23:58:47 -0300

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
